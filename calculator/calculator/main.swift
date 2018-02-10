4fc52ffdbd10fa4ae5f60e245b05bfd72b959a47
Admin <admin@Mac-Admin.local>
2018-02-11 00:29:30 +0300

    added files

--- calculator/calculator/main.swift
+++ calculator/calculator/main.swift
@@ -1,4 +1,13 @@
+//
+//  main.swift
+//  calculator
+//
+//  Created by Admin on 11.02.18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
 import Foundation
+
 
 var string = String(readLine()!)
 
